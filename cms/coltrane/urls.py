3d2c40df5bbf638540fcde7ff38a9753907f3479
matt <matthewlkiefer@gmail.com>
2013-04-22 14:58:26 -0500

    fixed some tagging stuff from last commit

--- cms/coltrane/urls.py
+++ cms/coltrane/urls.py
@@ -36,9 +36,9 @@ urlpatterns += patterns('',
 )
 
 # tag urls
-urlpattern += patterns('',
+urlpatterns += patterns('',
     url(r'^tags/$', 'django.views.generic.list_detail.object_list', {'queryset':Tag.objects.all()}),
-    url(r'^tags/entries/(?P<tag>[-\w]+)$', 'tagging.views.tagged_object_list',{'queryset_or_model':Entry,'template_name': 'coltrane/entries_by_tag.html'}),
-    url(r'^tags/links/(?P<tag>[-\w]+)$', 'tagging.views.tagged_object_list',{'queryset_or_model':Link,'template_name': 'coltrane/links_by_tag.html'}),
+    url(r'^tags/entries/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list',{'queryset_or_model':Entry,'template_name': 'coltrane/entries_by_tag.html'}),
+    url(r'^tags/links/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list',{'queryset_or_model':Link,'template_name': 'coltrane/links_by_tag.html'}),
 )
-)
+
