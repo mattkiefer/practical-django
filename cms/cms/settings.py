ceafd35e0bcb7c10a743c3fb2647ca6192a519e2
matt <matthewlkiefer@gmail.com>
2013-07-22 16:01:10 -0500

    comment app

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -127,6 +127,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django.contrib.comments',
+    'django.contrib.markup',
     'django.contrib.flatpages',
     'search',
     'coltrane',
