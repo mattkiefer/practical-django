60e30edbbd61fc042e6ca7b94786fc537c75a0b1
matt <matthewlkiefer@gmail.com>
2013-07-29 15:45:59 -0500

    typo: user_agent, not user-agent

--- cms/coltrane/models.py
+++ cms/coltrane/models.py
@@ -137,3 +137,46 @@ class Link(models.Model):
     
     def get_tags(self):
         return Tag.objects.get_for_object(self)
+        
+        
+        
+
+# start comment spam detector
+
+from django.utils import timezone
+from django.contrib.comments.models import Comment
+from django.db.models import signals
+from django.conf import settings
+
+from akismet import Akismet
+from django.contrib.sites.models import Site
+from django.utils.encoding import smart_str
+from django.contrib.comments.signals import comment_will_be_posted
+
+current_site = Site.objects.get_current()
+
+def moderate_comment(sender, comment, request, **kwargs):
+    if not comment.id:
+        entry = comment.content_object
+        delta = timezone.now() - entry.pub_date
+        if delta.days > 30:
+            # age detection
+            comment.is_public = False
+        else:
+            # akismet
+            # CHECK BLOG_URL
+            akismet_api = Akismet(key = settings.AKISMET_API_KEY, blog_url = 'http://%s/' %Site.objects.get_current().domain)
+            if akismet_api.verify_key():
+                akismet_data = {'comment_type': 'comment',
+                                'referrer': request.META['HTTP_REFERER'],
+                                'user_ip': comment.ip_address,
+                                'user_agent': request.META['HTTP_USER_AGENT'] }    
+                if akismet_api.comment_check(smart_str(comment.comment),
+                                             akismet_data,
+                                             build_data = True):                         
+                    comment.is_public = False
+
+comment_will_be_posted.connect(moderate_comment, sender=Comment)
+        
+# end comment spam detector
+
--- cms/coltrane/views.py
+++ cms/coltrane/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from coltrane.models import Category
 
+
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return render_to_response('coltrane/category_detail.html', {'object_list': category.live_entry_set.all(), 'category': category })
+
+
 
